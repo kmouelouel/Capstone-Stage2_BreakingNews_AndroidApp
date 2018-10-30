fa55f6313b6cc5010efc2aafa5f108071268975f
Kmouelouel <mouelouel@gmail.com>
2018-10-29 23:28:13 -0700

    hiding api keys

--- app/src/main/java/com/example/kmoue/breakingnews/MainActivity.java
+++ app/src/main/java/com/example/kmoue/breakingnews/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String API_KEY = BuildConfig.API_KEY;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

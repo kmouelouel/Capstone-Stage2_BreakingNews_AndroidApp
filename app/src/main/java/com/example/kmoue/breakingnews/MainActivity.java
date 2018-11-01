db2e1db276eccdc663712ec2a002e882b979d6f2
Kmouelouel <mouelouel@gmail.com>
2018-10-31 23:54:45 -0700

    added menu of categories

--- app/src/main/java/com/example/kmoue/breakingnews/MainActivity.java
+++ app/src/main/java/com/example/kmoue/breakingnews/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kmoue.breakingnews.utilities.NetworkUtils;
 
@@ -87,5 +90,51 @@ public class MainActivity extends AppCompatActivity {
             mTextViewResultsJson.setVisibility(View.INVISIBLE);
             mLoadingIndicator.setVisibility(View.INVISIBLE);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        //   Return true to display your menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        Context context = MainActivity.this;
+        String textToShow = "";
+        switch(itemThatWasClickedId){
+            case R.id.action_business:
+                textToShow ="Business clicked" ;
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_entertainment:
+                  textToShow = "Entertainment clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_general:
+                textToShow = "general clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_health:
+                textToShow = "health clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_science:
+                textToShow = "science clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_sports:
+                textToShow = "sports clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_technology:
+                textToShow = "technology clicked";
+                Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
